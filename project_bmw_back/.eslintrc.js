5012c1201bc2c9b82a55f13a23a84e3879ae1364
jae hyun <1994dbwogus@gmail.com>
2022-02-10 19:53:31 +0900

    build[back]: Nest 프로젝트 설정파일 수정 및 추가
    ### 수정
    - eslintrc.js : 규칙 변경
    - .prettierrc : 규칙 변경
    - package.json : 커멘드 추가
    - tsconfig.json : paths 추가
    
    ### 추가
    - Dockerfile

--- project_bmw_back/.eslintrc.js
+++ project_bmw_back/.eslintrc.js
@@ -4,10 +4,19 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
+  // ESLint 설정파일에, 충돌 방지 설정 추가
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'prettier',
+    'eslint:recommended',
     'plugin:prettier/recommended',
+  ],
+  // 적용규칙 : 밑에 있을 수록 우선순위를 가진다.
+  extends: [
+    'plugin:@typescript-eslint/recommended', //
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
@@ -20,5 +29,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // ESLint 설정파일에, .prettierrc에 설정된 값으로 충돌 방지 설정 추가
+    // eslint-config-prettier 모듈이 설치되어 있어야함
+    'prettier/prettier': ['error'],
   },
 };
